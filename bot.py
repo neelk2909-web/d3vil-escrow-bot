863e1b7a2277ada59f5b28bbf3c9962c9c384ee7
neelk2909-web <neelk2909@gmail.com>
2026-03-10 09:54:10 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+from telegram.ext import Updater, CommandHandler
+import os
+
+TOKEN = os.environ.get("8506879373:AAGqdWa3RNJUbatMJeEZy53hKtOdpLCz5C0")
+
+def start(update, context):
+    update.message.reply_text("🔥 Welcome to d3vil Escrow Bot")
+
+def help(update, context):
+    update.message.reply_text(
+        "Commands:\n/newdeal @seller amount"
+    )
+
+def newdeal(update, context):
+
+    seller = context.args[0]
+    amount = context.args[1]
+
+    update.message.reply_text(
+        f"Deal created with {seller} for {amount} USDT"
+    )
+
+updater = Updater(TOKEN, use_context=True)
+
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("help", help))
+dp.add_handler(CommandHandler("newdeal", newdeal))
+
+updater.start_polling()
+updater.idle()
